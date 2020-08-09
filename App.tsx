8b25ef435389a35335e6a0c0b0377f40517237dd
Arsalan Yaldram <arsalans@amdocs.com>
2020-08-09 12:45:13 +0530

    feat: added Eslint and Prettier Support, project setup Completed

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Lets get Started!</Text>
     </View>
   );
 }
@@ -14,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
