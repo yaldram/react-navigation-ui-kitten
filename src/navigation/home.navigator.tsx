c5bd2173a817a33e1b7f12dd8a5b4bcc33428273
Arsalan Yaldram <arsalans@amdocs.com>
2020-08-09 20:26:58 +0530

    feat: added Bottom Tab Navigator
    Added Bottom Tab Navigator For the Home Screen.

--- src/navigation/home.navigator.tsx
+++ src/navigation/home.navigator.tsx
@@ -1,29 +1,76 @@
 import {
+  createBottomTabNavigator,
+  BottomTabBarButtonProps,
+  BottomTabNavigationProp,
+  BottomTabBarProps,
+} from "@react-navigation/bottom-tabs";
+import {
   createDrawerNavigator,
   DrawerContentComponentProps,
   DrawerNavigationProp,
 } from "@react-navigation/drawer";
-import { RouteProp } from "@react-navigation/native";
+import { RouteProp, CompositeNavigationProp } from "@react-navigation/native";
 import React from "react";
 
-import { HomeIcon, InfoIcon } from "../assets/Icons";
+import { HomeIcon, InfoIcon, LayoutIcon, PersonIcon } from "../assets/Icons";
 import { HomeDrawerContent, Home, AboutScreen } from "../screens/home";
+import { HomeTabBar } from "../screens/home/HomeTabBar";
 import { AppRoute } from "./app.routes";
+import { ProfileNavigator } from "./profile.navigator";
+import { TodoNavigator } from "./todo.navigator";
 
 type HomeDrawerNavigatorParams = {
   [AppRoute.HOME]: undefined;
   [AppRoute.ABOUT]: undefined;
 };
 
+type HomeBottomTabsNavigatorParams = {
+  [AppRoute.TODO]: undefined;
+  [AppRoute.PROFILE]: undefined;
+};
+
+export type TodoTabNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<HomeBottomTabsNavigatorParams, AppRoute.TODO>,
+  DrawerNavigationProp<HomeDrawerNavigatorParams, AppRoute.HOME>
+>;
+
+export type ProfileTabNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<HomeBottomTabsNavigatorParams, AppRoute.PROFILE>,
+  DrawerNavigationProp<HomeDrawerNavigatorParams, AppRoute.HOME>
+>;
+
 const Drawer = createDrawerNavigator<HomeDrawerNavigatorParams>();
+const BottomTab = createBottomTabNavigator<HomeBottomTabsNavigatorParams>();
 
 export type DrawerHomeScreenProps = DrawerContentComponentProps & {
   navigation: DrawerNavigationProp<HomeDrawerNavigatorParams, AppRoute.HOME>;
 };
 
+export type BottonHomeScreenProps = BottomTabBarProps & {
+  navigation: TodoTabNavigationProp;
+};
+
 export interface AboutScreenProps {
   navigation: DrawerNavigationProp<HomeDrawerNavigatorParams, AppRoute.ABOUT>;
   route: RouteProp<HomeDrawerNavigatorParams, AppRoute.ABOUT>;
+}
+
+function HomeBottomNavigator() {
+  return (
+    // @ts-ignore: `tabBar` also contains a DrawerNavigationProp
+    <BottomTab.Navigator tabBar={HomeTabBar}>
+      <BottomTab.Screen
+        name={AppRoute.TODO}
+        component={TodoNavigator}
+        options={{ title: "TODO", tabBarIcon: LayoutIcon }}
+      />
+      <BottomTab.Screen
+        name={AppRoute.PROFILE}
+        component={ProfileNavigator}
+        options={{ title: "PROFILE", tabBarIcon: PersonIcon }}
+      />
+    </BottomTab.Navigator>
+  );
 }
 
 export function HomeNavigator() {
@@ -32,7 +79,7 @@ export function HomeNavigator() {
     <Drawer.Navigator drawerContent={HomeDrawerContent}>
       <Drawer.Screen
         name={AppRoute.HOME}
-        component={Home}
+        component={HomeBottomNavigator}
         options={{ title: "Home", drawerIcon: HomeIcon }}
       />
       <Drawer.Screen
---
+++ src/screens/home/HomeTabBar.tsx
@@ -0,0 +1,41 @@
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Divider,
+} from "@ui-kitten/components";
+import React from "react";
+
+import { SafeAreaLayout, SafeAreaInset } from "../../components/SafeAreaLayout";
+import { BottonHomeScreenProps } from "../../navigation/home.navigator";
+
+export function HomeTabBar(props: BottonHomeScreenProps) {
+  const onSelect = (index: number): void => {
+    const selectedTabRoute: string = props.state.routeNames[index];
+    props.navigation.navigate(selectedTabRoute);
+  };
+
+  const createNavigationTabForRoute = (route) => {
+    const { options } = props.descriptors[route.key];
+    return (
+      <BottomNavigationTab
+        key={route.key}
+        title={options.title}
+        // @ts-ignore: all Tab Screens strictly have UI Kitten Icon
+        icon={options.tabBarIcon}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaLayout insets={SafeAreaInset.BOTTOM}>
+      <Divider />
+      <BottomNavigation
+        appearance="noIndicator"
+        selectedIndex={props.state.index}
+        onSelect={onSelect}
+      >
+        {props.state.routes.map(createNavigationTabForRoute)}
+      </BottomNavigation>
+    </SafeAreaLayout>
+  );
+}
